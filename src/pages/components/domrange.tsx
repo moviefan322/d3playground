5dba8c3a47e9d6c08be38080d63305adcf92bfb2
moviefan322 <philman202@yahoo.com>
2023-12-21 11:12:55 -0500

    adds scaleBand

--- src/pages/components/domrange.tsx
+++ src/pages/components/domrange.tsx
@@ -8,7 +8,15 @@ interface BuildingData {
 
 const DomRange = () => {
   const svgRef = useRef<SVGSVGElement>(null);
-  const colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00"];
+  const colors = [
+    "#e41a1c",
+    "#377eb8",
+    "#4daf4a",
+    "#984ea3",
+    "#ff7f00",
+    "#ffff33",
+    "#a65628",
+  ];
 
   useEffect(() => {
     d3.json<BuildingData[]>("/data/buildingsMeters.json").then((data) => {
@@ -17,6 +25,15 @@ const DomRange = () => {
         d.height = Number(d.height);
         d.name = d.name;
       });
+
+      const x = d3
+        .scaleBand()
+        .domain(data!.map((d) => d.name))
+        .range([0, 400])
+        .paddingInner(0.3)
+        .paddingOuter(0.2);
+
+      console.log(x("Burj Khalifa"));
 
       const y = d3.scaleLinear().domain([0, 828]).range([0, 400]);
 
@@ -31,8 +48,8 @@ const DomRange = () => {
         .enter()
         .append("rect")
         .attr("y", 0)
-        .attr("x", (d, i) => i * 60 + 10)
-        .attr("width", 40)
+        .attr("x", (d, i) => x(d.name) || 0)
+        .attr("width", x.bandwidth)
         .attr("height", (d) => y(Number(d.height)))
         .attr("fill", (d, i) => colors[i]);
 
