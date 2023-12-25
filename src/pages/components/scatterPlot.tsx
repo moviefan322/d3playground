aa1a96448a0cc58882f7c4d439d3322a064bf13b
moviefan322 <philman202@yahoo.com>
2023-12-25 09:43:12 -0500

    adds x and y axis with scale, ticks, labels

--- src/pages/components/scatterPlot.tsx
+++ src/pages/components/scatterPlot.tsx
@@ -44,6 +44,55 @@ const ScatterPlot = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (data.length > 0) {
+      const data1800 = data[0].countries;
+
+      const maxLifeExp1800 = d3.max(data1800, (d) => d.life_exp);
+
+      const svg = d3.select(svgRef.current);
+      const g = svg
+        .append("g")
+        .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
+
+      // Update x-axis label
+      g.select(".x.axis-label")
+        .attr("x", WIDTH / 2)
+        .attr("y", HEIGHT + 110)
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .text("GDP Per Capita ($)");
+
+      // Update y-axis label
+      g.select(".y.axis-label")
+        .attr("x", -(HEIGHT / 2))
+        .attr("y", -60)
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)");
+
+      const x = d3.scaleLog([100, 150000], [0, WIDTH]);
+      const y = d3
+        .scaleLinear()
+        .domain([0, maxLifeExp1800 || 0])
+        .range([HEIGHT, 0]);
+
+      const xAxisCall = d3.axisBottom(x);
+      const xAxis = svg
+        .append("g")
+        .attr("class", "x axis")
+        .attr("transform", `translate(${MARGIN.LEFT}, ${HEIGHT + MARGIN.TOP})`);
+      xAxis.call(xAxisCall as any);
+
+      const yAxisCall = d3.axisLeft(y);
+      const yAxis = svg
+        .append("g")
+        .attr("class", "y axis")
+        .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
+      yAxis.call(yAxisCall as any);
+    }
+  }, [data, MARGIN, WIDTH, HEIGHT]);
+
   console.log(data);
 
   if (!data.length) return <p>Loading...</p>;
