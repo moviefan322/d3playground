faa2e124cfca3a887b7d9a76abee301388972cd7
moviefan322 <philman202@yahoo.com>
2023-12-24 23:41:21 -0500

    fixes label, data shfiting smoothely

--- src/pages/components/revenues.tsx
+++ src/pages/components/revenues.tsx
@@ -9,10 +9,9 @@ interface RevenueData {
 
 const Revenues = () => {
   const svgRef = useRef<SVGSVGElement>(null);
-  const [isProfit, setIsProfit] = useState<boolean>(true); // Boolean flag state
-  const [data, setData] = useState<RevenueData[]>([]); // Data state
-  const [months, setMonths] = useState<string[]>([]); // Months state
-  const [max, setMax] = useState<number>(0); // Max state
+  const [isProfit, setIsProfit] = useState<boolean>(true);
+  const [data, setData] = useState<RevenueData[]>([]);
+  const [months, setMonths] = useState<string[]>([]);
   const svgHeight = 400;
   const svgWidth = 600;
   const MARGIN = { LEFT: 100, RIGHT: 20, TOP: 10, BOTTOM: 130 };
@@ -21,36 +20,42 @@ const Revenues = () => {
 
   useEffect(() => {
     if (!svgRef.current) return;
+
     d3.csv("/data/revenues.csv", (d) => ({
       month: d.month,
       revenue: +d.revenue,
       profit: +d.profit,
     })).then((data: RevenueData[]) => {
       setMonths(data.map((d) => d.month));
-      setMax(
-        isProfit
-          ? d3.max(data, (d) => d.profit)!
-          : d3.max(data, (d) => d.revenue)!
-      );
       setData(data);
     });
   }, [isProfit]);
 
   useEffect(() => {
-    if (!svgRef.current || !data.length || !months.length || !max) return;
+    if (!svgRef.current || !data.length || !months.length) return;
 
-    // Clear all
-    d3.select(svgRef.current).selectAll("*").remove();
-
-    const svg = d3
-      .select(svgRef.current)
-      .append("svg")
-      .attr("width", svgWidth)
-      .attr("height", svgHeight);
-
+    const svg = d3.select(svgRef.current);
     const g = svg
-      .append("g")
+      .select("g")
       .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
+
+    const x = d3
+      .scaleBand()
+      .range([0, WIDTH])
+      .paddingInner(0.3)
+      .paddingOuter(0.2);
+
+    const y = d3.scaleLinear().range([HEIGHT, 0]);
+
+    const xAxisGroup = g
+      .append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0, ${HEIGHT})`);
+
+    const yAxisGroup = g
+      .append("g")
+      .attr("class", "y axis")
+      .attr("transform", `translate(0, 0)`);
 
     // X Label
     g.append("text")
@@ -66,124 +71,104 @@ const Revenues = () => {
       .append("text")
       .attr("class", "y axis-label")
       .attr("x", -(HEIGHT / 2))
-      .attr("y", -60)
+      .attr("y", -80)
       .attr("font-size", "20px")
       .attr("text-anchor", "middle")
       .attr("transform", "rotate(-90)");
 
-    const x = d3
-      .scaleBand()
-      .range([0, WIDTH])
-      .paddingInner(0.3)
-      .paddingOuter(0.2);
+    const color = d3
+      .scaleOrdinal()
+      .domain(months)
+      .range([
+        "#c11d1d",
+        "#eae600",
+        "#7ebd01",
+        "#f68a05",
+        "#f35e9a",
+        "#481e5d",
+        "#b10d6a",
+      ]);
 
-    const y = d3.scaleLinear().range([HEIGHT, 0]);
+    const value = isProfit ? "profit" : "revenue";
+    const maxValue = isProfit
+      ? d3.max(data, (d) => d.profit)
+      : d3.max(data, (d) => d.revenue);
 
-    const xAxisGroup = g
-      .append("g")
-      .attr("class", "x axis")
-      .attr("transform", `translate(0, ${HEIGHT})`);
+    x.domain(months);
+    y.domain([0, maxValue || 0]);
 
-    const yAxisGroup = g.append("g").attr("class", "y axis");
+    const xAxisCall = d3.axisBottom(x);
 
-    // d3.interval(() => {
-    //   setIsProfit((isProfit) => !isProfit);
-    //   update(data);
-    //   const newData = isProfit ? data : data.slice(1);
-    // }, 2000);
+    xAxisGroup
+      .transition()
+      .duration(750)
+      .call(xAxisCall as any)
+      .selectAll("text")
+      .attr("y", "10")
+      .attr("x", "-5")
+      .attr("text-anchor", "end")
+      .attr("transform", "rotate(-40)");
 
-    var update = (data: RevenueData[]) => {
-      const value = isProfit ? "profit" : "revenue";
+    const yAxisCall = d3
+      .axisLeft(y)
+      .ticks(5)
+      .tickFormat((d) => d + "m");
 
-      x.domain(months);
-      y.domain([0, max]);
+    yAxisGroup
+      .transition()
+      .duration(750)
+      .call(yAxisCall as any)
+      .selectAll("text")
+      .attr("font-size", "15px");
 
-      const xAxisCall = d3.axisBottom(x);
+    const rects = g.selectAll("rect").data(data);
 
-      xAxisGroup
-        .transition()
-        .duration(750)
-        .call(xAxisCall)
-        .selectAll("text")
-        .attr("y", "10")
-        .attr("x", "-5")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-40)");
+    rects
+      .exit()
+      .transition()
+      .duration(750)
+      .attr("height", 0)
+      .attr("y", y(0))
+      .remove();
 
-      const yAxisCall = d3
-        .axisLeft(y)
-        .ticks(5)
-        .tickFormat((d) => d + "m");
+    rects
+      .transition()
+      .duration(750)
+      .attr("x", (d) => x(d.month) || 0)
+      .attr("y", (d) => y(d[value]) || 0)
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => HEIGHT - y(d[value]))
+      .attr("fill", (d) => color(d.month) as string);
 
-      yAxisGroup.transition().duration(750).call(yAxisCall);
+    yLabel.text(isProfit ? "Profit ($)" : "Revenue ($)");
 
-      const color = d3
-        .scaleOrdinal()
-        .domain(months)
-        .range([
-          "#c11d1d",
-          "#eae600",
-          "#7ebd01",
-          "#f68a05",
-          "#f35e9a",
-          "#481e5d",
-          "#b10d6a",
-        ]);
-
-      // JOIN new data with old elements.
-      const rects = g.selectAll("rect").data(data);
-
-      // EXIT old elements not present in new data.
-      rects
-        .exit()
-        .attr("fill", "red")
-        .transition()
-        .duration(750)
-        .attr("height", 0)
-        .attr("y", y(0))
-        .remove();
-
-      // UPDATE old elements present in new data.
-      rects
-        .transition()
-        .duration(750)
-        .attr("x", (d) => x(d.month) || 0)
-        .attr("y", (d) => y(d[value]) || 0)
-        .attr("width", x.bandwidth())
-        .attr("height", (d) => HEIGHT - y(d[value]));
-
-      // ENTER new elements present in new data.
-      rects
-        .enter()
-        .append("rect")
-        .attr("x", (d) => x(d.month) || 0)
-        .attr("width", x.bandwidth())
-        .attr("fill", (d) => color(d.month) as string)
-        .attr("y", y(0))
-        .attr("height", 0)
-        .merge(
-          rects as unknown as d3.Selection<
-            SVGRectElement,
-            RevenueData,
-            SVGGElement,
-            unknown
-          >
-        )
-        .transition()
-        .duration(750)
-        .attr("y", (d) => y(d[value]) || 0)
-        .attr("height", (d) => HEIGHT - y(d[value]));
-
-      const text = isProfit ? "Profit ($)" : "Revenue ($)";
-      yLabel.text(text);
-    };
-
-    update(data);
-  }, [data, months, max, isProfit, MARGIN.LEFT, MARGIN.TOP, WIDTH, HEIGHT]);
+    rects
+      .enter()
+      .append("rect")
+      .attr("x", (d) => x(d.month) || 0)
+      .attr("width", x.bandwidth())
+      .attr("fill", (d) => color(d.month) as string)
+      .attr("y", y(0))
+      .attr("height", 0)
+      .merge(
+        rects as unknown as d3.Selection<
+          SVGRectElement,
+          RevenueData,
+          SVGGElement,
+          unknown
+        >
+      )
+      .transition()
+      .duration(750)
+      .attr("y", (d) => y(d[value]) || 0)
+      .attr("height", (d) => HEIGHT - y(d[value]));
+  }, [data, months, isProfit, WIDTH, HEIGHT]);
 
   return (
     <>
-      <svg ref={svgRef} height={svgHeight} width={svgWidth} />
+      <svg ref={svgRef} height={svgHeight} width={svgWidth}>
+        <g></g>
+      </svg>
       <button onClick={() => setIsProfit((isProfit) => !isProfit)}>
         Toggle
       </button>
