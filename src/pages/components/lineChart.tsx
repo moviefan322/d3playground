66d900b280ef5fd667aa921d74ca78bc6d007088
moviefan322 <philman202@yahoo.com>
2023-12-26 13:51:20 -0500

    successfully extracting data for coins

--- src/pages/components/lineChart.tsx
+++ src/pages/components/lineChart.tsx
@@ -1,31 +1,47 @@
 import React, { useRef, useEffect, useState } from "react";
 import * as d3 from "d3";
-interface DataPoint {
-  year: string;
-  value: number;
+
+interface CoinList {
+  [key: string]: CoinData[];
+}
+
+interface CoinData {
+  vol_24h: string | null;
+  date: string;
+  market_cap: string | null;
+  price_usd: string | null;
 }
 
 const LineChart = () => {
   const svgRef = useRef<SVGSVGElement>(null);
-  const [data, setData] = useState<DataPoint[]>([]);
+  const [data, setData] = useState<CoinList>();
   const svgWidth = 800;
   const svgHeight = 500;
-  const parseTime = d3.timeParse("%Y");
+  const parseDate = (dateString: string): Date | null => {
+    const [day, month, year] = dateString.split("/");
+    return new Date(Number(year), Number(month) - 1, Number(day));
+  };
   const bisectDate = d3.bisector((d) => (d as any).year).left;
 
   useEffect(() => {
-    d3.json<DataPoint[]>("data/example.json").then((data) => {
-      data!.forEach((d) => {
-        d.year =
-          parseTime(d.year)?.toISOString() || new Date(d.year).toISOString();
-        d.value = Number(d.value);
+    d3.json("data/coins.json").then((coinData: any) => {
+      const parsedCoinData = coinData as CoinList;
+
+      Object.keys(coinData).forEach((coinKey) => {
+        parsedCoinData[coinKey] = coinData[coinKey].map((coin: CoinData) => ({
+          ...coin,
+          date: coin.date ? parseDate(coin.date) : null,
+          vol_24h: coin.vol_24h ? +coin.vol_24h : null,
+          market_cap: coin.market_cap ? +coin.market_cap : null,
+          price_usd: coin.price_usd ? +coin.price_usd : null,
+        }));
       });
 
-      setData(data as DataPoint[]);
+      setData(parsedCoinData);
     });
   }, []);
 
-  if (!data.length) return <div>Loading...</div>;
+  if (!data) return <div>Loading...</div>;
   console.log(data);
 
   return (
